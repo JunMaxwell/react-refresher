26c50092b6bc499c80e0ab9cb7e7fac3352ddb69
Y.DoThien <haloany123@gmail.com>
2024-09-15 21:28:30 +0700

    refactor: change App.js to include header and main sections

--- essentials/src/App.js
+++ essentials/src/App.js
@@ -8,38 +8,49 @@ import React from 'react';
 // Except for outputting your custom component
 
 export function MainGoal() {
-  return <p>My main goal: Learn React in great detail</p>;
+    return <p>My main goal: Learn React in great detail</p>;
 }
 
 function App() {
-  return (
-    <div id="app">
-      <h1>Time to Practice!</h1>
-      <p>
-        Build a <code>&lt;MainGoal&gt;</code> component and insert it below this
-        text.
-      </p>
-      <p>
-        Your <code>&lt;MainGoal&gt;</code> component should simply output some
-        text that describes your main course goal (e.g., &quot;My main goal:
-        Learn React in great detail&quot;).
-      </p>
-      <p>
-        <strong>Important:</strong> You custom component must contain the text
-        &quot;My main goal:&quot;
-      </p>
-      <p>
-        <strong>Also important:</strong> For the automatic checks to succeed,
-        you <strong>must export</strong> your custom component function! Not as
-        a default but simply by adding the <code>export</code> keyword in front
-        of your function (e.g., <code>export function YOUR_COMPONENT_NAME</code>
-        ).
-      </p>
-      <MainGoal></MainGoal>
-      {/* DON'T CHANGE THE TEXT / CONTENT ABOVE */}
-      {/* OUTPUT YOUR COMPONENT HERE */}
-    </div>
-  );
+    return (
+        <div id="app">
+            <header>
+                <img src="assets/react-core-concepts.png" alt="Stylized atom" />
+                <h1>React Essentials</h1>
+                <p>
+                    Fundamental React concepts you will need for almost any app you are
+                    going to build!
+                </p>
+            </header>
+            <main>
+                <h2>Time to get started!</h2>
+                <h1>Time to Practice!</h1>
+                <p>
+                    Build a <code>&lt;MainGoal&gt;</code> component and insert it below this
+                    text.
+                </p>
+                <p>
+                    Your <code>&lt;MainGoal&gt;</code> component should simply output some
+                    text that describes your main course goal (e.g., &quot;My main goal:
+                    Learn React in great detail&quot;).
+                </p>
+                <p>
+                    <strong>Important:</strong> You custom component must contain the text
+                    &quot;My main goal:&quot;
+                </p>
+                <p>
+                    <strong>Also important:</strong> For the automatic checks to succeed,
+                    you <strong>must export</strong> your custom component function! Not as
+                    a default but simply by adding the <code>export</code> keyword in front
+                    of your function (e.g., <code>export function YOUR_COMPONENT_NAME</code>
+                    ).
+                </p>
+                <MainGoal></MainGoal>
+                {/* DON'T CHANGE THE TEXT / CONTENT ABOVE */}
+                {/* OUTPUT YOUR COMPONENT HERE */}
+            </main>
+        </div>
+    );
 }
 
 export default App;
